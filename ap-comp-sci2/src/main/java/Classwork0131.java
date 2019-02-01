a7085f416f569764dbf041a1b2443779e1424f64
CreativePenguin <panda501wp@gmail.com>
2019-02-01 03:00:11 -0500

    add gradle project to organize compiled java files, and the apcs 2 folder

---
+++ ap-comp-sci2/src/main/java/Classwork0131.java
@@ -0,0 +1,33 @@
+package src.main.java;
+
+public class Classwork0131 {
+
+    //map(lamda x : x == 694, data)
+    private static int sequentialSearch(int[] data, int x) {
+        for(int i = 0; i < data.length; i++) {
+            if(data[i] == x) return i;
+        }
+        return -1;
+    }
+    //function: data.length is the amount of work done
+
+    public static void print(int [] data){
+        for (int x: data)
+            System.out.print(x + " ");
+        System.out.println();
+    }
+
+    private static boolean selfOrganizingSearch(int[] data, int x) {
+        boolean val = false;
+        int count = 0;
+        for(int i : data) {
+            if(i == x) {
+                count ++;
+                val = true;
+            }
+        }
+        for(int i = 0; i < count; i++) data[i] = x;
+        for(int i = data.length - 1; i >= count; i--) data[i] = data[i - count];
+        return val;
+    }
+}
---
+++ ap-comp-sci2/src/main/java/Hw2.java
@@ -0,0 +1,16 @@
+package src.main.java;
+
+public class Hw2 {
+
+    private static boolean isSorted(int[] data) {
+        int prev = data[0];
+        for(int i : data) {
+            if(i < prev) {
+                return false;
+            }
+            prev = i;
+        }
+        return true;
+    }
+
+}
