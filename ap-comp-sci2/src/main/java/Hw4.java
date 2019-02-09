df6ec08c6305b0c1b6ef5868c2104c9b0d379e10
CreativePenguin <panda501wp@gmail.com>
2019-02-08 20:12:04 -0500

    add code for week 1 of hw

---
+++ ap-comp-sci2/src/main/java/Hw4.java
@@ -0,0 +1,48 @@
+package src.main.java;
+
+public class Hw4 {
+
+    public class Anagrams {
+
+        public String randomString(int n) {
+            String ans = "";
+            for (int i = 0; i < n ; i++) {//O(n)
+                int r = 'a' + (int)(Math.random() * ('z' - 'a')); //O(1)
+                ans +=  (char)r; //O(1)
+            }
+            
+            return ans;
+        }
+
+        public String shuffle(String s) {
+            String ans = "";
+            int n = s.length();
+            for (int i = 0; i < n; i++) { //O(n)
+                int r = (int)(Math.random() * s.length());//O(1)
+                ans += s.substring(r,r+1); //O(1)
+                s = s.substring(0,r) + s.substring(r+1); //O(1)
+            }
+            return ans;
+        }
+
+
+
+        public boolean isAnagram(String a, String b) {
+            if(a.length() != b.length()) return false;
+            for(int i = 0; i < b.length(); i++) {
+                int tempVal = b.indexOf(a.substring(i, i + 1));
+                if(tempVal ==  -1) return false;
+                b = b.substring(0, tempVal) + (tempVal + 1);
+            }
+            return true;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Hw4.Anagrams hw = new Hw4().new Anagrams();
+        hw.isAnagram("tacocat", "cattaco");
+        hw.isAnagram("Stuypulse", "StewiePlus");
+        hw.isAnagram("door", "odor");
+    }
+}
---
+++ ap-comp-sci2/src/main/java/Hw5.java
@@ -0,0 +1,89 @@
+package src.main.java;
+
+import java.util.Arrays;
+public class Hw5 {
+    public class Anagrams{
+
+        // O(N)
+        // StringBuffer is a mutable sequence of characters.
+        // A StringBuffer Object is not testable material.
+        public String randomString(int n){
+            StringBuffer s = new StringBuffer("");
+            for (int i = 0; i < n; i++){
+                int r = 'a' + (int)(Math.random() * ('z' - 'a')); // O(1)
+                s.append((char)r); // O(1)
+            }
+            // convert the mutable StringBuffer to an immutable String
+            return s.toString();
+        }
+
+        // O(N)
+        public String shuffle(String s){
+            StringBuffer sb = new StringBuffer(s); // O(n)
+            int n = s.length();
+            for (int i = 0; i < n; i++){
+                int r = (int)(Math.random() * (n - i)) + i; // O(1)
+                char c1 = sb.substring(r,r+1).charAt(0); // O(1)
+                char c2 = sb.substring(i,i+1).charAt(0); // O(1)
+                sb.setCharAt(i,c1);  // O(1) sb is mutable
+                sb.setCharAt(r, c2); // O(1)
+            }
+            return sb.toString();
+        }
+
+        // O(n^2) and not O(nlogN)
+        public boolean isAnagram(String a, String b){
+            if (a.length() != b.length())   // O(1)
+                return false; 
+            // O(N^2)
+            for (int i = 0; i < a.length(); i++){ // O(N)
+                int pos = b.indexOf(a.substring(i,i+1)); // O(N)
+                if (pos == -1) return false;
+                b = b.substring(0,pos) + b.substring(pos + 1); // O(N)
+            }
+            return true;
+        }
+
+        // O(NlogN) and not O(n)
+        public boolean isAnagramV2(String a, String b){
+            if (a.length() != b.length()) return false;
+            final int N = a.length();
+            String [] aArray = new String[N];   // O(N)
+            String [] bArray = new String[N];   // O(N)
+            // O(N)
+            for (int i = 0; i < N; i++){        
+                aArray[i] =  a.substring(i,i+1);    // O(1)
+                bArray[i] =  b.substring(i,i+1);    // O(1)
+            }
+            Arrays.sort(aArray);       // O(NlogN)
+            Arrays.sort(bArray);       // O(NlogN)
+        
+            for (int i = 0; i < N; i++) // O(N)
+                if (!aArray[i].equals(bArray[i])) return false;
+            // Arrays.equals(aArray, bArray);
+            return true;
+        }
+
+        // O(N)
+        public boolean isAnagramV3(String a, String b){
+            int[] list = new int[26];
+            for(int i = 0; i < a.length(); i++) {
+                list[a.charAt(i)]++;
+            }
+            for(int i = 0; i < b.length(); i++) {
+                list[b.charAt(i)]--;
+            }
+            for(int i : list) {
+                if(i != 0) return false;
+            }
+            return true;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Hw5.Anagrams val = new Hw5().new Anagrams();
+        val.isAnagramV3("stuypulse", "pulsestuy");
+    }
+
+}
