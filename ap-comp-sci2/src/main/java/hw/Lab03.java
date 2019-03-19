a6772d94e9a54347eb9fc495a3b6d51b8b0ab966
Winston Peng <panda501wp@gmail.com>
2019-03-19 18:12:53 -0400

    Finish Lab03. Change build.gradle to compile lab 3.

--- ap-comp-sci2/src/main/java/hw/Lab03.java
+++ ap-comp-sci2/src/main/java/hw/Lab03.java
@@ -2,17 +2,17 @@ package hw;
 
 public final class Lab03 {
 
-    public <N> N getParent(N[] li, int idx) {
-        return li[(int) Math.ceil(idx / 2.0) / 2];
+    public Number getParent(Number[] li, int idx) {
+        return li[(int) Math.ceil(idx / 2.0) - 1];
     }
 
-    public <N> int getHeapSize(N[] li) {
+    public int getHeapSize(Number[] li) {
         int val = 0;
         while(li.length > Math.pow(2, val)) val++;
         return val;
     }
 
-    public <N> N getLargestNonLeafIndex(N[] li) {
+    public Number getLargestNonLeafIndex(Number[] li) {
         return li[(int) Math.pow(2, getHeapSize(li))];
     }
 
@@ -22,16 +22,16 @@ public final class Lab03 {
 
     public class BinaryHeap{
 
-        public void print(int [] data){
-            for (int x :  data)
+        public void print(Number[] data){
+            for (Number x :  data)
                 System.out.print(x + " ");
-            System.out.println():
+            System.out.println();
         }
 
         // pre: data != null and both i and j are index positions
         // post: the elements data[i] and data[j] are exchanged.
-        public void swap(int [] data, int i, int j){
-            int temp = data[i];
+        public void swap(Number[] data, int i, int j){
+            Number temp = data[i];
             data[i] = data[j];
             data[j] = temp;
         }
@@ -42,26 +42,29 @@ public final class Lab03 {
         //      up to a point.
         // Post: returns true if data has heap property
         //       false otherwise.
-        public boolean isHeap(int [] data){
-            return isHeapHelper(data,0);
+        public boolean isHeap(Number[] data){
+            return isHeapHelper(data,data.length - 1);
         }
 
-        private boolean isHeapHelper(int[] data, int i){
-            isHeapHelper(data, i++);
-            return getParent(data, i)
-        }
-
-
-        public void run() {
-            int [] bheap = {5,1,3};
-            print(bheap);
-
+        private boolean isHeapHelper(Number[] data, int i) {
+            if(getParent(data, i).doubleValue() < data[i].doubleValue()) {
+                return false;
+            }
+            else if(i > 1) {
+                return isHeapHelper(data, --i);
+            }
+            else {
+                return true;
+            }
         }
 
     }
 
     public static void main(String[] args) {
-
+        Lab03.BinaryHeap l3 = new Lab03().new BinaryHeap();
+        System.out.println(l3.isHeap(new Number[] {1, 2, 3, 4, 5, 6, 7}));
+        System.out.println(l3.isHeap(new Number[] {7, 6, 5, 4, 3, 2, 1}));
+        System.out.println(l3.isHeap(new Number[] {7, 6, 5, 4, 3, 1, 2}));
     }
 
 }
