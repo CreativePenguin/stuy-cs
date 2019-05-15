f30aaa6c657291dd6162d0681453b930586470c6
Winston Peng <panda501wp@gmail.com>
2019-05-15 09:23:37 -0400

    Fix print statement

--- ap-comp-sci2/src/rust/src/main.rs
+++ ap-comp-sci2/src/rust/src/main.rs
@@ -4,5 +4,5 @@ use int_bst::IntBST;
 fn main() {
     let mut bst = IntBST::new();
     bst.add(10); bst.add(15); bst.add(3);
-    println!("{:#?}", bst);
+    bst.inorder; bst.preorder; bst.postorder;
 }
