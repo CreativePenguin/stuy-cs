088c3c06e0b4c78f452fc2e4376e08dbcf7539ad
Winston Peng <panda501wp@gmail.com>
2019-05-15 09:23:06 -0400

    Fixing adds & adding prints
    Try to make recursive add no longer get ownership. Add iterative add (will need to test), and also add the other ways to print (other than inorder).

--- ap-comp-sci2/src/rust/src/int_bst.rs
+++ ap-comp-sci2/src/rust/src/int_bst.rs
@@ -13,26 +13,27 @@ impl IntBST {
         }
     }
 
-    pub fn add(mut self, x:isize) {
-        self.root = self.addH(x, self.root);
+    pub fn addR(&mut self, x:isize) {
+        self.addH(x, self.root);
         self.size += 1;
     }
 
-    fn addH(&self, x:isize, rt:Option<Box<TreeNode>>) -> Option<Box<TreeNode>> {
+    fn addH(&self, x:isize, rt:&Option<Box<TreeNode>>) -> &Option<Box<TreeNode>> {
         if let Some(node) = rt {
             if x <= (*node).val {
                 Some(Box::new(
                     TreeNode {
                         val: x,
-                        left: self.addH(x, (*node).left),
-                        right: (*node).right,
-                    }))
+                        left: *self.addH(x, (*node).left),
+                        right: node.right,
+                    }
+                ))
             } else {
                 Some(Box::new(
                     TreeNode {
                         val: x,
-                        left: (*node).left,
-                        right: self.addH(x, (*node).right),
+                        left: node.left,
+                        right: *self.addH(x, (*node).right),
                     }
                 ))
             }
@@ -40,19 +41,62 @@ impl IntBST {
             Some(Box::new(TreeNode::new(x)))
         }
     }
+    
+    pub fn addI(&mut self, x:isize) {
+        let mut leader = self.root;
+        while(!leader.isLeaf()) {
+            if(x <= leader.val) {
+                leader = leader.left;
+            } else {
+                leader = leader.right;
+            }
+        }
+        if(x <= leader.val) {
+            leader.left = TreeNode::new(x);
+        } else {
+            leader.right = TreeNode::new(x);
+        }
+        size += 1;
+    }
 
     pub fn inorder(&self) {
         self.inorderH(self.root);
         println!();
     }
 
-    fn inorderH(&self, rt:Option<Box<TreeNode>>) {
+    fn inorderH(&self, rt:&Option<Box<TreeNode>>) {
         if let Some(node) = rt {
             self.inorderH(node.left);
             println!("{} ", node.val);
             self.inorderH(node.right);
         }
 
+    }
+    
+    fn preorder(&self) {
+        self.preorderH(self.root);
+        println!();
+    }
+    
+    fn preorderH(&self, rt:&Option<Box<TreeNode>>) {
+        if let Some(node) = rt {
+            println!("{} ", node.val);
+            self.inorderH(node.left);
+            self.inorderH(node.right);
+        }
+    }
+    
+    fn postorder(&self) {
+        self.postorderH(self.root);
+        println!();
+    }
+    
+    fn preorderH(&self, rt:&Option<Box<TreeNode>>) {
+        if let Some(node) = rt {
+            self.preorderH(node.left);
+            self.preorderH(node.right);
+            println!("{} ", node.val);
+        }
     }
 
 }
@@ -73,5 +117,8 @@ impl TreeNode {
         }
     }
 
+    fn isLeaf() -> bool {
+        left == None && right == None
+    }
 }
 
