2220a9ef85f7ccb4b6c3d042a7c04075aab5f109
CreativePenguin <panda501wp@gmail.com>
2019-09-25 11:48:13 -0400

    aaaaahhhhh

--- lesson_09_arrays/main.c
+++ lesson_09_arrays/main.c
@@ -4,17 +4,19 @@ int main() {
     int ARR2_SIZE = 10;
     int arr[ARR_SIZE];
     int arr2[ARR2_SIZE];
-    int poi = arr;
-    int poi2 = arr2;
+    int *poi = arr;
+    printf("pointer to arr is %d\n", poi);
+    int *poi2 = arr2;
     int i;
+    srand(time(NULL));
     for(i = 0; i < ARR_SIZE; i = i + 1) {
-        arr[i] = srand(time(NULL));
-        printf("Random value for %d, is %d", i, arr[i]);
+        arr[i] = rand();
+        printf("Random value for arr %d, is %f\n", i, arr[i]);
     }
     arr[ARR_SIZE - 2] = 0;
     for(i = 0; i < ARR_SIZE; i = i + 1) {
-        *(poi2 + i) = *(poi + i);
-        printf("Random value for %d, is %d", i, );
+        poi2 = &(poi + i);
+        printf("Random value for arr2 %d, is %d\n", i, );
     }
     return 0;
 }
