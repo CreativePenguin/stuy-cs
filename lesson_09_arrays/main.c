5bad16712c36cbc2a989909c87e2a70cd0f6abcd
Winston Peng <panda501wp@gmail.com>
2019-09-26 17:41:04 -0400

    Finish main.c. Finally works for lesson 9

--- lesson_09_arrays/main.c
+++ lesson_09_arrays/main.c
@@ -1,23 +1,16 @@
 #include <stdio.h>
 int main() {
-    int ARR_SIZE = 10;
-    int ARR2_SIZE = 10;
-    int arr[ARR_SIZE];
-    int arr2[ARR2_SIZE];
-    int *poi = arr;
-    printf("pointer to arr is %d\n", poi);
-    int *poi2 = arr2;
-    int i;
-    srand(time(NULL));
-    for(i = 0; i < ARR_SIZE; i = i + 1) {
-        arr[i] = rand();
-        printf("Random value for arr %d, is %f\n", i, arr[i]);
-    }
-    arr[ARR_SIZE - 2] = 0;
-    for(i = 0; i < ARR_SIZE; i = i + 1) {
-        poi2 = &(poi + i);
-        printf("Random value for arr2 %d, is %d\n", i, );
-    }
-    return 0;
+  const int SIZE = 10;
+  int arr[SIZE], arr2[SIZE];
+  int* poi = arr;
+  int* poi2 = arr2;
+  for(int i = 0; i < SIZE; i++) {
+    *(poi + i) = rand();
+    printf("%d : %d \n", i, *(poi + i));
+  }
+  for(int i = 0; i < SIZE; i++) {
+    *(poi2 + i) = *(poi + i);
+    printf("%d : %d \n", i, *(poi2 + i));
+  }
 }
 
